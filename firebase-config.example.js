362884687e92e935e91ed960689f2ddc84cbd108
iasteven2025-art <iasteven2025@gmail.com>
2026-08-22 21:17:41 -0300

    Publicar firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,14 @@
+window.VENDEGOV_FIREBASE_CONFIG = {
+  enabled: true,
+  tenantId: "computeck-demo",
+  allowSignup: false,
+  fallbackLocal: true,
+  firebase: {
+    apiKey: "SUA_API_KEY",
+    authDomain: "vendegov-crm-computeck.firebaseapp.com",
+    projectId: "vendegov-crm-computeck",
+    storageBucket: "vendegov-crm-computeck.firebasestorage.app",
+    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+    appId: "SEU_APP_ID",
+  },
+};
